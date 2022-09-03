789a0f2fcf9010eb5975a60d30c42bb33238e89b
Semih <monitorminor28@gmail.com>
2022-09-03 11:27:46 +0300

    first commit

--- src/components/TodoComponent.js
+++ src/components/TodoComponent.js
@@ -5,11 +5,11 @@ function Todo() {
   const [todoValue, setTodoValue] = useState("");
   return (
     <>
-    <input onChange={(e) => setTodoValue(e.target.value)} value={todoValue} type={'text'} placeholder='Todo giriniz...'></input>
-    <button type={'submit'}  onClick={() => setTodo([...todo, todoValue])}>Ekle</button>
+    <input onChange={(e) => setTodoValue(e.target.value)} value={todoValue} type={'text'} placeholder='Enter todo...'></input>
+    <button type={'submit'}  onClick={() => setTodo([...todo, todoValue])}>Add new todo</button>
     <ul>
       {todo.map(td => (
-        <li>{td}</li>
+        <li>{td} <button type={'submit'}>Delete</button></li>
       ))}
     </ul>
     </>
